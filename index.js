4ce565a0d37c8bff25136800ac6f3aed4cdd4251
Kevin Kamto <kamtokevin@gmail.com>
2019-05-11 23:53:29 -0400

    feat: set up basic index.js config -- now serves static files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const Express = require('express');
+const express = require('express');
+const path = require('path');
+const connect = require('connect');
+const serveStatic = require('serve-static')
 
-let app = Express();
+let app = express();
+let port = process.env.PORT || 3004;
+let host = process.env.HOST || "localhost";
 
-app.get('/', () => {
+app.use(express.static('.'));
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, "/", "index.html"))
+})
 
+
+app.listen(port, (err) =>{
+    console.log(`[Server] Live on ${host}:${port}`)
+    if (err) throw err
 })
