ff776d3d91b7a55c113ea30b89e25a39c6098f3f
Kevin Kamto <kamtokevin@gmail.com>
2019-05-18 13:52:49 -0400

    feat: added blog redirect

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ let port = process.env.PORT || 3004;
 let host = process.env.HOST || "http://localhost";
 
 app.use(express.static('.'));
+
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, "/", "index.html"))
 })
 
+app.get('/blog', (req, res) => {
+    //Send link redirect for blog
+    res.send("google.com");
+})
 
 app.listen(port, (err) =>{
     console.log(`[Server] Live on ${host}:${port}`)
