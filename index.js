a2fa363c32ef6a34255c184fe230ae6fee8a8c90
Kevin Kamto <kamtokevin@gmail.com>
2019-05-22 03:09:36 -0400

    feat: added store routing

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ let port = process.env.PORT || 3004;
 let host = process.env.HOST || "http://localhost";
 
 app.use(express.static('.'));
+
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, "/", "index.html"))
 })
 
+app.get('/store', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/", "/store"))
+})
 
 app.listen(port, (err) =>{
     console.log(`[Server] Live on ${host}:${port}`)
